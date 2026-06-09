00a19be25813b96a838ab5e4570d15470f2ce0f1
protein53 <proteinfiftythree53@gmail.com>
2026-06-08 20:57:26 -0600

    Update install_my_fave_apps_and_dots.sh

--- install_my_fave_apps_and_dots.sh
+++ install_my_fave_apps_and_dots.sh
@@ -1,9 +1,4 @@
 bash ./install_my_favourite_apps.sh
-cd /tmp
-git clone https://github.com/npquintos/dotfiles.git
-cd /tmp/Linux-Installation
-bash ./link_dot_files.sh
-cp /tmp/dotfiles/make_link_to_dotfiles.sh /usr/local/bin/.
-chmod a+x /usr/local/bin/make_link_to_dotfiles.sh
+git clone https://github.com/protein53/dotfiles ~/dotfiles
 wget https://github.com/ryanoasis/nerd-fonts/blob/master/install.sh -O - | bash
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+
