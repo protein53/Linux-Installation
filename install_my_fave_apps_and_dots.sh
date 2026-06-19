e18a47f18d430f8162da1016f24ee7a1a33da563
protein53 <proteinfiftythree53@gmail.com>
2026-06-18 20:31:37 -0600

    Update install_my_fave_apps_and_dots.sh

--- install_my_fave_apps_and_dots.sh
+++ install_my_fave_apps_and_dots.sh
@@ -1,6 +1,6 @@
 bash install_favourite_apps.sh
-bash install_flatpaks.sh &
-bash miscellaneous.sh &
+bash install_flatpaks.sh&
+bash miscellaneous.sh&
 git clone https://github.com/protein53/dotfiles ~/dotfiles
 cd ~/dotfiles
 bash prestow.sh
